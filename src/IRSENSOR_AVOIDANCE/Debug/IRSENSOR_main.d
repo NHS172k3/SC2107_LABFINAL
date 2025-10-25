00ca46136e7949fff3164433aec04c38389e2812
NHS172k3 <harinachiappansubramanian@gmail.com>
2025-10-25 10:48:32 +0800

    Bump.c i was a retard and added everything in irq handler but rest should be fine

---
+++ src/IRSENSOR_AVOIDANCE/Debug/IRSENSOR_main.d
@@ -0,0 +1,43 @@
+# FIXED
+
+IRSENSOR_main.obj: ../IRSENSOR_main.c
+IRSENSOR_main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h
+IRSENSOR_main.obj: C:/ti/ccsv7/ccs_base/arm/include/msp.h
+IRSENSOR_main.obj: C:/ti/ccsv7/ccs_base/arm/include/msp432p401r.h
+IRSENSOR_main.obj: C:/ti/ccsv7/ccs_base/arm/include/msp_compatibility.h
+IRSENSOR_main.obj: C:/ti/ccsv7/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+IRSENSOR_main.obj: C:/ti/ccsv7/ccs_base/arm/include/msp432p401r_classic.h
+IRSENSOR_main.obj: C:/ti/ccsv7/ccs_base/arm/include/CMSIS/core_cm4.h
+IRSENSOR_main.obj: C:/ti/ccsv7/ccs_base/arm/include/CMSIS/cmsis_compiler.h
+IRSENSOR_main.obj: C:/ti/ccsv7/ccs_base/arm/include/system_msp432p401r.h
+IRSENSOR_main.obj: ../../inc/Clock.h
+IRSENSOR_main.obj: ../../inc/CortexM.h
+IRSENSOR_main.obj: ../../inc/IRDistance.h
+IRSENSOR_main.obj: ../../inc/TimerA1.h
+IRSENSOR_main.obj: ../../inc/UART0.h
+IRSENSOR_main.obj: ../../inc/LaunchPad.h
+IRSENSOR_main.obj: ../../inc/ADC14.h
+IRSENSOR_main.obj: ../../inc/LPF.h
+IRSENSOR_main.obj: ../../inc/Motor.h
+IRSENSOR_main.obj: ../../inc/PWM.h
+
+../IRSENSOR_main.c: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h: 
+C:/ti/ccsv7/ccs_base/arm/include/msp.h: 
+C:/ti/ccsv7/ccs_base/arm/include/msp432p401r.h: 
+C:/ti/ccsv7/ccs_base/arm/include/msp_compatibility.h: 
+C:/ti/ccsv7/ccs_base/arm/include/CMSIS/cmsis_ccs.h: 
+C:/ti/ccsv7/ccs_base/arm/include/msp432p401r_classic.h: 
+C:/ti/ccsv7/ccs_base/arm/include/CMSIS/core_cm4.h: 
+C:/ti/ccsv7/ccs_base/arm/include/CMSIS/cmsis_compiler.h: 
+C:/ti/ccsv7/ccs_base/arm/include/system_msp432p401r.h: 
+../../inc/Clock.h: 
+../../inc/CortexM.h: 
+../../inc/IRDistance.h: 
+../../inc/TimerA1.h: 
+../../inc/UART0.h: 
+../../inc/LaunchPad.h: 
+../../inc/ADC14.h: 
+../../inc/LPF.h: 
+../../inc/Motor.h: 
+../../inc/PWM.h: 
